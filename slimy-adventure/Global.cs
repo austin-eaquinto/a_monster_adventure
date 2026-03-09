b159f2a31878f51fe55fdb6cdb32e78580eedcf4
David Ugas Mora <ugasmoradavid@gmail.com>
2026-03-09 17:20:42 -0600

    Made the menu prettier

--- slimy-adventure/Global.cs
+++ slimy-adventure/Global.cs
@@ -1,22 +1,34 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class Global : Node
 {
+	
+	public static Global instance { get; private set; }
+
 	public string NextScene;
 	
 	[Signal]
 	public delegate void AlertGuardsEventHandler(Vector2 guardPosition, Character spottedPrisoner);
 	
-	// public void TransitionScene(, bool game){
+	public Dictionary<string,string> sceneDict = new Dictionary<string, string>
+	{
+    	["NA"] = "",
+    	["LoadingScreen"] = "res://screens/loading/loading_screen.tscn",
+    	["LoadingScreen"] = "res://screens/loading/loading_screen.tscn"
+	};
+
+	public void TransitionScene(string sceneName){
 		
-	// 	GetTree().ChangeSceneToFile("res://screens/loading/loading_screen.tscn");
-	// }
+		GetTree().ChangeSceneToFile(sceneDict[sceneName]);
+	}
 	
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		instance = this;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
