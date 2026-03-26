7bdcee92920b3761cb5d6a4380f133567ac7c455
NatalieTesam <nataliekay1023@gmail.com>
2026-03-26 15:07:45 -0600

    animated text

--- slimy-adventure/screens/endings/ending/Ending.cs
+++ slimy-adventure/screens/endings/ending/Ending.cs
@@ -1,49 +1,67 @@
 using Godot;
 using System;
+using Godot.Collections;
 
 public partial class Ending : Control
 {
-	private Label finishTime;
-	private Label endMessage;
-	private Array<Ally> allies;
-	
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		finishTime = GetNode<Label>("VBoxContainer/Time");
-		endMessage = GetNode<Label>("VBoxContainer/Message");
-		allyCount = Global.player.followingAllies
-		allyCount = 0;
-		displayResults();
-	}
+    private Label finishTime;
+    private Label endMessage;
+    private Label congratsMessage;
+    private Array<Ally> allies = [];
+    
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        finishTime = GetNode<Label>("VBoxContainer/Time");
+        endMessage = GetNode<Label>("VBoxContainer/Message");
+        congratsMessage = GetNode<Label>("VBoxContainer/Label");
+        // allies = Global.Instance.player.followingAllies;
+        displayAllies();
+        displayResults();
+        
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
-	
-	public void displayResults() {
-		// change the text based on how many allies escaped
-		// animate how the text displays slowly
-		// switch to credits scene
-		
-		finishTime.Text = "";
-		
-		if (allyCount == 3) {
-			endMessage.Text = "Yay all of you escaped together!";
-		}
-		else if (allyCount == 2) {
-			endMessage.Text = "You made it out with 2 friends";
-		}
-		else if (allyCount == 1) {
-			endMessage.Text = "You made it out with a friend";
-		}
-		else {
-			endMessage.Text = "One is better than none...";
-		}
-	}
-	
-	
-	
-	
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
+    
+    public void displayResults() {
+        // change the text based on how many allies escaped
+        // switch to credits scene
+        
+        finishTime.Text = "Time: Placeholder";
+        finishTime.VisibleRatio = 0.0f;
+        
+        if (allies.Count == 3) {
+            endMessage.Text = "Well done, You are strongest together!";
+        }
+        else if (allies.Count == 2) {
+            endMessage.Text = "Now we know who your least favorite was...";
+        }
+        else if (allies.Count == 1) {
+            endMessage.Text = "I guess one is better than none...";
+        }
+        else {
+            endMessage.Text = "Wow... so much for teamwork...";
+        }
+        endMessage.VisibleRatio = 0.0f;
+        
+        var tween = CreateTween();
+        var charReadRate = 0.1f;
+
+        tween.TweenProperty(congratsMessage, "visible_ratio", 1.0f, congratsMessage.Text.Length * charReadRate).From(0.0f);
+        tween.TweenInterval(1.0f); 
+        tween.TweenProperty(endMessage, "visible_ratio", 1.0f, endMessage.Text.Length * charReadRate);
+        tween.TweenInterval(1.0f); 
+        tween.TweenProperty(finishTime, "visible_ratio", 1.0f, finishTime.Text.Length * charReadRate);
+    }
+    
+    public void displayAllies() {
+        foreach (Ally ally in allies) {
+            AnimatedSprite2D allySprite = ally.GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+            GD.Print(allySprite);
+        }
+    }
+    
 }
