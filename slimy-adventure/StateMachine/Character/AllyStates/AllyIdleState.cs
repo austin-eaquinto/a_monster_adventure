a451d652459b875adf4a4967f112f11d0a255099
Kim Josell <145394112+kimjosell@users.noreply.github.com>
2026-04-01 15:06:10 -0600

    Adding dialogues

--- slimy-adventure/StateMachine/Character/AllyStates/AllyIdleState.cs
+++ slimy-adventure/StateMachine/Character/AllyStates/AllyIdleState.cs
@@ -2,9 +2,12 @@ using Godot;
 using System;
 using System.Drawing;
 
+
 [GlobalClass]
 public partial class AllyIdleState : CharacterState
 {
+
+	bool isPlayerClose = false;
 
 	[Export]
 	public Area2D joinArea { get; set;} = null;
@@ -18,9 +21,41 @@ public partial class AllyIdleState : CharacterState
 	{
 		base._Ready();
 		joinArea.BodyEntered += BodyEnters;
+		joinArea.BodyExited += BodyExited;
+		var dialogic = GetNode("/root/Dialogic");
+		dialogic.Connect("signal_event", Callable.From<string>(OnDialogicSignal));
 	}
 
-	
+	private void OnDialogicSignal(string argument)
+	{
+		if(argument == "follow_player")
+		{
+			isPlayerClose = false;
+			(character as Ally).state = Ally.AllyStates.PerfectFollow;
+		}
+	}
+
+    public override void _Process(double delta)
+    {
+        if(isPlayerClose && Input.IsActionJustPressed("interact"))
+		{
+			run_dialogue((character as Ally).id);
+		}
+    }
+
+	public void run_dialogue(int id)
+	{
+		if(id == 0)
+		{
+			var dialogic = GetNode("/root/Dialogic");
+			dialogic.Call("start", "ghostHello");
+		}
+	}
+
+	protected void BodyExited(Node2D body)
+	{
+		isPlayerClose = false;
+	}
 
 	protected void BodyEnters(Node2D body)
 	{
@@ -35,6 +70,7 @@ public partial class AllyIdleState : CharacterState
 		else
 		{
 			(character as Ally).state = Ally.AllyStates.PerfectFollow;
+			isPlayerClose = true;
 		}
 	}
 
